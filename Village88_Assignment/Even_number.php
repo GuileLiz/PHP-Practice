0a2d6b085678834672e11d0c9f5dc2f2773d98c0
LizGuile <lizguile19@gmail.com>
2023-01-29 00:41:43 +0800

    PHP

---
+++ Village88_Assignment/Even_number.php
@@ -0,0 +1,5 @@
+<?php
+    for($number=1; $number<=1000; $number++) {
+        echo $number . "<br>";
+    }
+?>
---
+++ Village88_Assignment/Sequence_of_8.php
@@ -0,0 +1,5 @@
+<?php
+    for($number=8;$number<=1000;$number+=8) {
+        echo $number . "<br>";
+    }
+?>
