68273209e157f9e20fd836c7b74d93592b559c94
LizGuile <lizguile19@gmail.com>
2023-02-16 12:43:43 +0800

    Simulating Foreach 3

---
+++ Village88_Assignment/Simulating_Foreach.php
@@ -0,0 +1,24 @@
+<!-- <?php
+$list = array(2,4,6,8);
+foreach($list as $key => $value){
+		echo $key . " - " . $value ."<br />";
+	}
+?>
+// 0-2 1-4 2-6 3-8 -->
+
+<!-- <?php
+$list = array(2,4,6,8);
+foreach($list as $value){
+     echo $value ."<br />";
+}
+// 2 4 6 8	
+?> -->
+
+<?php
+$fruits= array("A" => "Apple", "B" => "Banana");
+foreach($fruits as $key => $value)
+{
+     echo $key . " - " . $value ."<br />";
+}
+//
+?>
