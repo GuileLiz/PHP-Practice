ccc8735ba203e9a6450153ece49e80df1eb4370e
LizGuile <lizguile19@gmail.com>
2023-02-17 23:51:01 +0800

    multi array

---
+++ Village88_Assignment/multiarray.php
@@ -0,0 +1,16 @@
+<?php
+$exams = array(
+ array(
+  'title' => 'Web Fundamentals Track Exam',
+  'language' => 'HTML &amp; CSS'
+ ),
+ array(
+  'title' => 'PHP Track Exam',
+  'language' => 'PHP using CodeIgniter, Ajax'
+ ),
+ array(
+  'title' =>'JS Track Exam',
+  'language' => 'Express, Sockets'
+ )
+);
+?>
