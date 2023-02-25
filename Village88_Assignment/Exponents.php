5898585bcd2aa805920ef52a06903785465f06fe
LizGuile <lizguile19@gmail.com>
2023-02-25 22:52:40 +0800

    Exponents

---
+++ Village88_Assignment/Exponents.php
@@ -0,0 +1,19 @@
+<?php
+$digits = array(3, 12, 30);
+function exponential($arr)
+{
+	$result = []
+	foreach($arr as num) {
+		$result_push[num]
+		return $result
+  }
+}
+$result = exponential($digits)
+var_dump($result); 
+/* expected output:
+        array (size=3)
+        0 => int 27
+        1 => int 1728
+        2 => int 27000
+*/
+?>
