fe4460e05eb87c2158aa86812a7999b88bd2d373
LizGuile <lizguile19@gmail.com>
2023-02-28 23:58:12 +0800

    Bingo

---
+++ Village88_Assignment/Bingo.php
@@ -0,0 +1,68 @@
+<html>
+<head>
+	<style>
+		table, th, td { 
+			border: 1px solid black;
+			margin: 0;
+		}
+		table {
+			padding:0;
+		}
+		.odd-row {
+			color:blue;
+		}
+		.even-row {
+			color:red
+		}
+		.even-row, .odd-row {
+			text-align:center;
+		}
+	</style>
+</head>
+<body>
+	<table>
+		<tr>
+			<th class="odd-row">B</th>
+			<th class="odd-row">I</th>
+			<th class="odd-row">N</th>
+			<th class="odd-row">G</th>
+			<th class="odd-row">O</th>
+		</tr>
+		<tr>
+			<td class="even-row">2</td>
+			<td class="even-row">4</td>
+			<td class="even-row">6</td>
+			<td class="even-row">8</td>
+			<td class="even-row">10</td>
+		</tr>
+		<tr>
+			<td class="odd-row">3</td>
+			<td class="odd-row">6</td>
+			<td class="odd-row">9</td>
+			<td class="odd-row">12</td>
+			<td class="odd-row">15</td>
+		</tr>
+		<tr>
+			<td class="even-row">4</td>
+			<td class="even-row">8</td>
+			<td class="even-row">12</td>
+			<td class="even-row">16</td>
+			<td class="even-row">20</td>
+		</tr>
+		<tr>
+			<td class="odd-row">5</td>
+			<td class="odd-row">10</td>
+			<td class="odd-row">15</td>
+			<td class="odd-row">20</td>
+			<td class="odd-row">25</td>
+		</tr>
+		<tr>
+			<td class="even-row">6</td>
+			<td class="even-row">12</td>
+			<td class="even-row">18</td>
+			<td class="even-row">24</td>
+			<td class="even-row">30</td>
+		</tr>
+	</table>
+</body>
+</html>
