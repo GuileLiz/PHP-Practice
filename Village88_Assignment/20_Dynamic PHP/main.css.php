2048ad09a1a7bbda92fd1d434ca9274338fd5cfc
LizGuile <lizguile19@gmail.com>
2023-04-17 23:29:02 +0800

    Dynamics part 2

--- Village88_Assignment/20_Dynamic PHP/main.css.php
+++ Village88_Assignment/20_Dynamic PHP/main.css.php
@@ -1,16 +1,11 @@
 <?php
-header("Content-type: text/css");
-?> 
-<!-- If you have PHP code in a CSS file, for it to be read by PHP interpreter  -->
+ header('Content-type: text/css');
+?>
 p {
-	font-size: <?=rand(10,20);?>px;
+	font-size: <?=rand(10,30);?>px;
 	color: rgb(<?=rand(0,255);?>,<?=rand(0,255);?>,<?=rand(0,255)?>);
 }
 em {
 	font-size: <?=rand(10,30);?>px;
 	color: rgb(<?=rand(0,255);?>,<?=rand(0,255);?>,<?=rand(0,255)?>);
 }
-
-
-
-
