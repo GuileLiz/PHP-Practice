fc3881d10c8de30329b88ecca6b3ace2910881b8
LizGuile <lizguile19@gmail.com>
2023-04-23 02:01:09 +0800

    finalizng csv to html

--- Village88_Assignment/21_ExamTo HTML/21_ExcelToHTML.php
+++ Village88_Assignment/21_ExamTo HTML/21_ExcelToHTML.php
@@ -1,7 +1,47 @@
-<?php
-$file = fopen("us-500.csv","r");
-while(! feof($file)) {
-	print_r(fgetcsv($file));
-}
-fclose("us-500.csv","r");
-?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Excel To HTML</title>
+		<style>
+			table, th, td { 
+				border: 1px solid black;
+				margin: 0;
+			}
+			table {
+				border-spacing:0;
+			}
+		</style>
+	</head>
+	<body>
+		<table>
+			<tr>
+	<?php
+		$file = fopen("us-500.csv","r");
+		while(! feof($file)) {
+			foreach(fgetcsv($file) as $value=>$key){
+				if ($value%11!=0 ) { ?>
+					<td><?=$key?></td>
+				
+			</tr>
+		</table>
+		<?php 
+				}
+			}
+		} ?>
+		</body>
+</html>
+<!-- 	<?php
+		$file = fopen("us-500.csv","r");
+		while(! feof($file)) {
+			foreach(fgetcsv($file) as $value=>$key){ //
+				if ($value%11==0 && $value>0) { //////
+					echo("<br/[>");
+				} else {
+					echo(" ".$key);
+				}
+			}
+		}
+		fclose("us-500.csv"); -->
